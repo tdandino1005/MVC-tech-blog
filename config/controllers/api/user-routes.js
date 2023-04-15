22c9604da77751240705b8263d94073ebfd0c450
Tony Dandino <tdandino1005@gmail.com>
2023-04-15 13:35:34 -0500

    Created route to create a new user

---
+++ config/controllers/api/user-routes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Route to create a new user
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
+
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route to login a user
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'No user found with this username!' });
+            return;
+        }
+
+        const validPassword = await user.checkPassword(req.body.password);
+    }
+    if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+    }
+    req.session.save(() => {
+        req.session.user_id = user.id;
+        req.session.username = user.username;
+        req.session.loggedIn = true;
+
+        res.json({ user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json({ message: "No user found!" });
+  }
+});
+
+// Route to logout a user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
