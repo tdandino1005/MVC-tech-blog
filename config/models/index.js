e434b23a95280c4987a55b2646fe505b50eda01e
Tony Dandino <tdandino1005@gmail.com>
2023-04-14 22:13:44 -0500

    Imported all models and created associations

--- config/models/index.js
+++ config/models/index.js
@@ -1,4 +1,41 @@
-const User = require('./user');
-const Post = require('./post');
-const Comment = require('./comment');
+// import all models here
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Post belongsTo User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// User has many Comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Comment belongsTo User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+// Comment belongsTo Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+// export all models
+module.exports = { User, Post, Comment };
