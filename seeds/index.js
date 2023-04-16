7161c63fc93264bd0bb9d79661233bb71812f3de
Tony Dandino <tdandino1005@gmail.com>
2023-04-16 17:19:59 -0500

    testing fixes in deploying

--- seeds/index.js
+++ seeds/index.js
@@ -2,9 +2,9 @@
 const sequelize = require('../config/connection');
 
 // import all of the seed data from the data folder
-const seedUser = require('./userData');
-const seedPost = require('./postData');
-const seedComments = require('./commentData');
+const seedUser = require('./user-seeds');
+const seedPost = require('./post-seeds');
+const seedComments = require('./comment-data');
 
 // Sync the database and seed the tables
 const seedAll = async () => {
