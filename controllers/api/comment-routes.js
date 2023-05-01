d8169bfafce373913e10a5243e08ddbb7434c908
Tony Dandino <tdandino1005@gmail.com>
2023-04-30 20:51:31 -0500

    Connection issues

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // GET all comments
 // Routing to /api/comments
