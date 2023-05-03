b04d1f37fdc6b47e7f67916332912e63da550c60
Tony Dandino <tdandino1005@gmail.com>
2023-05-02 20:20:44 -0500

    Updated seed names and path

---
+++ seeds/userData.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'John Doe',
+        password: 'password12345',
+    },
+    {
+        username: 'Jane Doe',
+        password: 'password012345',
+    },
+    {
+        username: 'John Smith',
+        password: 'password123456',
+    },
+    {
+        username: 'Jane Smith',
+        password: 'password1234567',
+    },
+
+];
+
+const seedUser = () => User.bulkCreate(userdata,
+    {
+        individualHooks: true,
+    });       
+
+module.exports = seedUser;
