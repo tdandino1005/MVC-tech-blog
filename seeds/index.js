e8e37cada9439b2f3ebea2ae351920b3d16aa156
Tony Dandino <tdandino1005@gmail.com>
2023-05-02 20:22:08 -0500

    Fixed import of seed data

--- seeds/index.js
+++ seeds/index.js
@@ -2,9 +2,9 @@
 const sequelize = require('../config/connection');
 
 // import all of the seed data from the data folder
-const seedUser = require('./user-seeds');
-const seedPost = require('./post-seeds');
-const seedComments = require('./comment-data');
+const seedUser = require('./userData');
+const seedPost = require('./postData');
+const seedComments = require('./commentData');
 
 // Sync the database and seed the tables
 const seedAll = async () => {
