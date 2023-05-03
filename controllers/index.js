470f8a3fd261be2ce5a31c1d0d635300f57a6aca
Tony Dandino <tdandino1005@gmail.com>
2023-05-02 20:27:29 -0500

    Fixed controller imports

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 // import all controllers
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
